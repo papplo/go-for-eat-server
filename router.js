ad71ab38e00e6c8dd3278f9d489aa1a082b073e8
Andrea Ceccarelli <gltch2003@gmail.com>
2018-02-03 14:01:59 +0100

    style: fix lint style in router

--- router.js
+++ router.js
@@ -14,59 +14,59 @@ const Events = db.get('events');
 const Users = db.get('users');
 
 // Geo Indexing for MongoDb 
-Events.createIndex( { location : "2dsphere" } );
+Events.createIndex( { location : '2dsphere' } );
 
 const eventsController = new EventsController(Events, monk);
 const usersController = new UsersController(Users, Events, monk);
 
 
 const authorize = async (ctx, next) => {
-	if (!ctx.user) {
-		ctx.status = 401;
-		return;
-	}
+  if (!ctx.user) {
+    ctx.status = 401;
+    return;
+  }
 
-	await next();
+  await next();
 };
 
 const routes = function (app) {
-	router
-		.post('/api/v1/auth', usersController.auth)
-		.get('/api/v1/users/:id', authorize, usersController.getUser.bind(usersController))
-		.get('/api/v1/me', authorize, usersController.me.bind(usersController))
-		.put('/api/v1/me', authorize, usersController.edit.bind(usersController))
-		.put('/api/v1/users/:id', authorize, ratingsController.rating.bind(usersController))
+  router
+    .post('/api/v1/auth', usersController.auth)
+    .get('/api/v1/users/:id', authorize, usersController.getUser.bind(usersController))
+    .get('/api/v1/me', authorize, usersController.me.bind(usersController))
+    .put('/api/v1/me', authorize, usersController.edit.bind(usersController))
+    .put('/api/v1/users/:id', authorize, ratingsController.rating.bind(usersController))
 
-		.post('/api/v1/events', authorize, eventsController.createEvent.bind(eventsController))
-		.put('/api/v1/events/:id', authorize, eventsController.editEvent.bind(eventsController))
-		.delete('/api/v1/events/:id', authorize, eventsController.deleteEvent.bind(eventsController))
-		.get('/api/v1/events/:id', authorize, eventsController.getEvent.bind(eventsController))
-		.put('/api/v1/events/:id/users', authorize, eventsController.joinEvent.bind(eventsController))
-		.delete('/api/v1/events/:id/users', authorize, eventsController.leaveEvent.bind(eventsController))
-		.get('/api/v1/events', authorize, eventsController.getEvents.bind(eventsController))
+    .post('/api/v1/events', authorize, eventsController.createEvent.bind(eventsController))
+    .put('/api/v1/events/:id', authorize, eventsController.editEvent.bind(eventsController))
+    .delete('/api/v1/events/:id', authorize, eventsController.deleteEvent.bind(eventsController))
+    .get('/api/v1/events/:id', authorize, eventsController.getEvent.bind(eventsController))
+    .put('/api/v1/events/:id/users', authorize, eventsController.joinEvent.bind(eventsController))
+    .delete('/api/v1/events/:id/users', authorize, eventsController.leaveEvent.bind(eventsController))
+    .get('/api/v1/events', authorize, eventsController.getEvents.bind(eventsController))
 
-		.options('/', options)
-		.trace('/', trace)
-		.head('/', head);
+    .options('/', options)
+    .trace('/', trace)
+    .head('/', head);
 
-	app
-		.use(router.routes())
-		.use(router.allowedMethods());
+  app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
-	return app;
+  return app;
 };
 
 
 const head = async () => {
-	return;
+  return;
 };
 
 const options = async () => {
-	this.body = 'Allow: HEAD,GET,PUT,DELETE,OPTIONS';
+  this.body = 'Allow: HEAD,GET,PUT,DELETE,OPTIONS';
 };
 
 const trace = async () => {
-	this.body = 'Smart! But you can\'t trace.';
+  this.body = 'Smart! But you can\'t trace.';
 };
 
 module.exports = routes;
